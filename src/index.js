2100b941ebf04e73bfdc26f55f8b6933ac21014f
Abdurazaq333 <irazaqr986@gmail.com>
2024-07-31 13:11:55 +0500

    database connected also add utils functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDataBase from "./db/database.js";
 
 dotenv.config();
+
+connectDataBase();
 
 app.listen(process.env.PORT, () => {
   console.log(`app is running on port ${process.env.PORT}`);
