e73a8bff01d2d10392962eff07bbe4773ad6881d
Abdurazaq333 <irazaqr986@gmail.com>
2024-08-01 23:48:05 +0500

    total expesnse & total income controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -30,5 +30,63 @@ const getTransaction = asyncHandler(async (req, res) => {
 
   return res.status(200).json(new ApiResponse(200, transaction, "data posted"));
 });
+//@dec ---------getTotalExpense details--------------------
+const getTotalExpense = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
 
-export { postTransaction, getTransaction };
+  const totalExpenses = await Transaction.aggregate([
+    {
+      $match: {
+        user: userId,
+        type: "expense",
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        TotalExpense: {
+          $sum: "$amount",
+        },
+      },
+    },
+    {
+      $project: {
+        TotalExpense: 1,
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, totalExpenses, "total of expenses"));
+});
+//@dec ---------getTotalExpense details--------------------
+const getTotalIncome = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+
+  const totalIncomes = await Transaction.aggregate([
+    {
+      $match: {
+        user: userId,
+        type: "income",
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalIncome: {
+          $sum: "$amount",
+        },
+      },
+    },
+    {
+      $project: {
+        totalIncome: 1,
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, totalIncomes, "total of income"));
+});
+
+export { postTransaction, getTransaction, getTotalExpense, getTotalIncome };
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -3,11 +3,15 @@ const router = Router();
 import {
   postTransaction,
   getTransaction,
+  getTotalExpense,
+  getTotalIncome,
 } from "../controllers/transaction.controller.js";
 
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 router.route("/").post(verifyJwt, postTransaction);
 router.route("/").get(verifyJwt, getTransaction);
+router.route("/expense").get(verifyJwt, getTotalExpense);
+router.route("/income").get(verifyJwt, getTotalIncome);
 
 export default router;
